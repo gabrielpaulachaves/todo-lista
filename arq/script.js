f7725a9987ce028a1c2e445e91ac09246df414ac
Gabriel <bielpchaves@gmail.com>
2026-04-16 10:11:10 -0300

    melhorando as coisas

--- arq/script.js
+++ arq/script.js
@@ -1,16 +1,12 @@
 const botao = document.getElementById("bTnadd")
-
+const paredetarefas = document.getElementById("tarefas")
 
 botao.addEventListener("click", ()=>{
     console.log("funcionando")
-    const texto =document.getElementById("texto")
+    const texto = document.getElementById("texto")
     const div = document.createElement("div")
-    div.textContent = texto.value
+    div.append(texto.value)
     div.style.width = '100px'
-    document.getElementById("tarefas").append(div)
-
-
-
-
-
+    div.style.height = 
+    paredetarefas.prepend(div)
 })
